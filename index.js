857ec8f38c5afd7f95212683c1e0553e7c056574
taufikka <taufikul67@gmail.com>
2021-10-27 02:24:30 +0600

    added port

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const cors = require('cors');
 require('dotenv').config();
 
 const app = express();
-const port = 5000;
+const port = process.env.PORT || 5000;
 
 // middleware
 app.use(cors())
@@ -17,7 +17,7 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
 async function run() {
-    try{
+    try {
         await client.connect();
 
         const database = client.db('carMechanic')
@@ -31,9 +31,9 @@ async function run() {
         })
 
         // GET single service
-        app.get('/services/:id', async(req,res)=>{
+        app.get('/services/:id', async (req, res) => {
             const id = req.params.id;
-            console.log('getting specific id',id)
+            console.log('getting specific id', id)
             const query = { _id: ObjectId(id) }
             const service = await serviceCollection.findOne(query)
             res.json(service)
@@ -45,29 +45,29 @@ async function run() {
             console.log('hit the post api', service)
 
             const result = await serviceCollection.insertOne(service);
-             console.log(result);
+            console.log(result);
             res.send(result)
         })
 
         // DELETE api
-        app.delete('/services/:id', async(req, res) => {
+        app.delete('/services/:id', async (req, res) => {
             const id = req.params.id;
-            const query = {_id: ObjectId(id)};
+            const query = { _id: ObjectId(id) };
             const result = await serviceCollection.deleteOne(query);
             res.json(result)
         })
 
     }
-    finally{
+    finally {
         // await client.close();
     }
 }
 run().catch(console.dir);
 
-app.get('/', (req, res)=> {
+app.get('/', (req, res) => {
     res.send('Running Genius Server');
 })
 
-app.listen(port, () =>{
+app.listen(port, () => {
     console.log('Running Genius Server on port', port)
 })
